bc1fa14e7e0f7e7cd63d153e200e3c97940e086d
영준 <ernmqq@gmail.com>
2025-09-02 20:08:15 +0900

    9/2일 이희만 강사님 수업 + 야자복습

---
+++ leeTeacher/Spring-workspace/myspringpjtnew/src/myspring/EmpServiceImpl.java
@@ -0,0 +1,28 @@
+package myspring;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component("empService")
+public class EmpServiceImpl implements EmpService {
+	
+	//@Autowired
+	EmpDAO dao;
+	
+	public EmpServiceImpl(EmpDAO dao) {
+		this.dao = dao;
+	}
+
+	@Override
+	public List<Emp> getEmpListByDeptNo(int deptNo) throws Exception {
+		return dao.getEmpListByDeptNo(deptNo);
+	}
+
+	@Override
+	public List<Emp> getEmpByFirstNLastName(String firstName, String lastName, String fullName) throws Exception {
+		return dao.getEmpByFirstNLastName(firstName, lastName, fullName);
+	}
+	
+}
---
+++ leeTeacher/Spring-workspace/myspringpjtnew/src/project/EmpService.java
@@ -0,0 +1,23 @@
+package project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public interface EmpService {
+
+	public List<Emp> findEmpByYear(int year) throws Exception;
+	
+	public List<Emp> findEmpByDeptId(int deptId) throws Exception;
+	
+	public List<Emp> findEmpByName(String firstName) throws Exception;
+	
+	public List<EmpStat> printStat() throws Exception;
+	
+	public List<Emp> findEmpByManagerName(String departname) throws Exception;
+	
+	public List<Emp> findByCity(String city) throws Exception;
+	
+}
